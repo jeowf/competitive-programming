e80415ee7fa5f70d6dca00476d82d202d7d8ca3f
jeowf <moraisfelipe97@gmail.com>
2019-02-15 17:40:39 -0300

    c

--- 01/c/c.cpp
+++ 01/c/c.cpp
@@ -6,15 +6,18 @@ int main(){
 	std::cin >> t;
 
 	//long a,b,c;
+	std::cout << t << ":" << std::endl;
 
 	for (int i = 2; i < t; i++){
 		for (int j = 1; j >= 0; j--){
 			int a = i;
 			int b = a - j;
 
-			if ( ((float) t/(a+b)) == ((float) t)  || 
-				((float) t/(a+b)) == ((float) t) )
-			std::cout << a << ", " << b << std::endl; 
+			int t1 = ( (t/(a+b))*(a+b) );
+			int t2 = ((t/(a+b)) + 1)*a + ((t/(a+b)))*b;
+
+			if (t1 == t || t2 == t)
+				std::cout << a << ", " << b << std::endl; 
 		}
 		
 	}
