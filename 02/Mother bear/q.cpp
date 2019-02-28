9e2b6fff4f0258d860b0aec5fa03cec7acd5848a
jeowf <moraisfelipe97@gmail.com>
2019-02-28 14:43:19 -0300

    02-q2

--- 02/Mother bear/q.cpp
+++ 02/Mother bear/q.cpp
@@ -1,29 +1,53 @@
 #include <iostream>
 #include <cctype>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    /*
-    char ch;
-    unsigned m = 10;
+bool is_char( char x ){
+    return x != '.' && 
+           x != ',' && 
+           x != '!' &&
+           x != ' ' &&
+           x != '?';
+}
 
-    int i = 0;
-    while (cin && m--) {
-        cin.read(&ch, sizeof(ch));
-        i++;
-        if (ch == '\n')
-        	cout << "OPA" << endl;
-        cout << i << ": " << ch << endl;
+int main(int argc, char *argv[]){
+    string s = "";
+
+    while (1){
+        getline(cin, s);
+        if (s == "DONE") break;
+
+        int i = 0, f = s.size() - 1;   
+
+        bool p = true;
+
+        while (i != f && s.size()>0){
+            if (is_char(s[i])){
+                if (is_char(s[f])){
+
+                    if (tolower(s[i]) != tolower(s[f])){
+                        p = false;
+                        break;
+                    }
+
+                    i++; f--;
+                } else {
+                    f--;
+                }
+            } else {
+                i++;
+            }
+        }
+        
+
+        if (p)
+            cout << "You won't be eaten!" << endl;
+        else
+            cout << "Uh oh.." << endl;
+
     }
-    return 0;
 
-*/
-    string s;
-    while (cin >> s && s!= "DONE"){
-
-        for ()
-    }
     return 0;
 }
