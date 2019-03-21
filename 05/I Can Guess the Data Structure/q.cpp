5c3209c1509bc17f65f7432f2889db8545e9fad2
jeowf <moraisfelipe97@gmail.com>
2019-03-21 17:41:56 -0300

    another one

--- 05/I Can Guess the Data Structure/q.cpp
+++ 05/I Can Guess the Data Structure/q.cpp
@@ -4,100 +4,70 @@ using namespace std;
 
 int main(){
 
-
 	int n;
 
 	while (cin >> n){
 
-		map<int, int> m;
-		stack < pair<int, int> > v;
-		int order = 1;
+		stack<int> s;
+		queue<int> q;
+		priority_queue<int> pq;
 
-		bool error = false;
 
 		bool qu = true, 
 			 st = true, 
 			 pr = true;
 
-		int op, x;
+		int op, v;
 
 		for (int i = 0; i < n; ++i) {
-			cin >> op >> x;
+			cin >> op >> v;
+
 			if (op == 1){
-				m[x] = order++;
-
-				pair<int, int> p;
-				p.first = x; // value
-				p.second = m[x]; // ordem de entrada
-
-				v.push(p);
+				if (qu) q.push(v);
+				if (st) s.push(v);
+				if (pr) pq.push(v);
 
 			} else {
-				if (m[x] == 0)
-					error = true;
-				//order--;
-				m[x] = 0;
-				
-				pair<int, int> aux, next;
-				if (!v.empty()){
-					aux = v.top();
-					v.pop()
-					if (!v.empty()){
-						next = v.top();
-
-						if (v[i-1].second < v[i].second)
-							st = false;
-						if (v[i-1].second > v[i].second)
-							qu = false;
-						if (v[i-1].first < v[i].first)
-							pr = false;
-					}
-
-
+				if (qu) {
+					if (q.empty() || q.front() != v)
+						qu = false;
+					else
+						q.pop();
 				}
 
-					
+				if (st) {
+					if (s.empty() || s.top() != v)
+						st = false;
+					else
+						s.pop();
+				}
 				
-
-				v.pop_back();
-
+				if (pr) {
+					if (pq.empty() || pq.top() != v)
+						pr = false;
+					else
+						pq.pop();
+				}
 			}
 		}
 
-		/*for (int i = 0; i < v.size(); ++i)
-			{
-				cout << v[i].first << " ";
-			}
-			cout << endl;*/
 
-		if (!error){
+		if (qu && !st && !pr){
+			cout << "queue" << endl;
+		} else if (st && !qu && !pr){
+			cout << "stack" << endl;
 
-			/*if (v.size() != order-1){
-				qu = st = false;
-			}*/
+		} else if (pr && !qu && !st){
+			cout << "priority queue" << endl;
 
-			if (qu && !st && !pr){
-				cout << "queue" << endl;
-			} else if (st && !qu && !pr){
-				cout << "stack" << endl;
-
-			} else if (pr && !qu && !st){
-				cout << "priority queue" << endl;
-
-			} else if (!pr && !st && !qu){
-				cout << "impossible" << endl;
-				//impossivel
-			} else {
-				cout << "not sure" << endl;
-
-			}
-		} else {
+		} else if (!pr && !st && !qu){
 			cout << "impossible" << endl;
-			
+			//impossivel
+		} else {
+			cout << "not sure" << endl;
+
 		}
-
 		
-
 	}
 
 	return 0;
