3c7d4acd8f5d5ad39d94b038a512029776f4daa6
jeowf <moraisfelipe97@gmail.com>
2019-04-25 17:34:36 -0300

    gegeo eh viado

--- 08/Sum it up/q.cpp
+++ 08/Sum it up/q.cpp
@@ -3,39 +3,43 @@
 using namespace std;
 
 int t, n;
-int numbers[12];
+int numbers[13];
 
-bool mask[12];
+bool mask[13];
 bool has_solution = false;
+
+map<string, int> exps;
 
 //map <bool[12], bool> last_results;
 
-void calc_sum(int prev, int index){
+void calc_sum(int prev, int index, int s){
 	//used[index] = prev;
 
 	if (prev > t || index >= n)
 		return;
 
+	stringstream expr;
 	// lembrar de ajeitar: printar em funcao do tamanho da mask
 	if (prev == t){
 		has_solution = true;
-			cout << "index : " << index << "\n";
-			for (int i = 0; i <= index; ++i)	{
-				if (mask[i]){
-					cout << numbers[i];
-					if (i < index-1) 
-						cout << "+";
-				}
-			}
-			cout << endl;
 
-		//return;
+		int j = 0;
+		for (int i = 0; i <= index; ++i){
+			if (mask[i]){
+				expr << numbers[i];
+				if (j++ < s-1) 
+					expr << "+";
+
+			}
+		}
+		exps[expr.str()] = 1;
 	}
 
+
 	mask[index] = true;
-	calc_sum(prev + numbers[index], index + 1);
+	calc_sum(prev + numbers[index], index + 1,s+1);
 	mask[index] = false;
-	calc_sum(prev, index + 1);
+	calc_sum(prev, index + 1,s);
 
 	return;
 
@@ -48,15 +52,29 @@ int main(){
 			mask[i] = false;
 			cin >> numbers[i];
 		}
+		numbers[n] = 0;
+		n++;
 
 		has_solution = false;
 
 		cout << "Sums of " << t << ":\n";
 		
-		calc_sum(0,0);
+		calc_sum(0,0,0);
 
 		if (!has_solution)
 			cout << "NONE\n";
+
+		/*for(auto & e : exps){
+			if (e.second == 1){
+				cout << e.first << endl;
+			}
+		}*/
+		for (auto it = exps.rbegin(); it != exps.rend(); ++it){
+			if (it->second == 1)
+				cout << it->first << endl;
+		}
+
+		exps.clear();
 
 	}
 
