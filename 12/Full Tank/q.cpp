a9973533e4a8cf3b8eccf58550b6474a680f24ac
jeowf <moraisfelipe97@gmail.com>
2019-06-06 19:43:59 -0300

    full tank

---
+++ 12/Full Tank/q.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	
+	int n, m;
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; ++i) {
+		
+	}
+
+	return 0;
+}
